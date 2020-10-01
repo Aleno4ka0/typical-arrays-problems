3898eca7aa90328f5defa793e8d44697bcebf8d3
Aleno4ka0 <68109635+Aleno4ka0@users.noreply.github.com>
2020-10-01 13:25:36 +0400

    feat: add, min. max, avg.

--- src/index.js
+++ src/index.js
@@ -1,12 +1,40 @@
 
 exports.min = function min (array) {
-  return 0;
+  if(array == undefined || array.length == 0){
+    return 0
+  }
+  var minim = array[0]
+    for(var i = 0; i < array.length; i++){
+      if((array[i]) < minim){
+        minim = array[i]
+      }
+    }
+    return minim
 }
 
+
 exports.max = function max (array) {
-  return 0;
+  if(array == undefined || array.length == 0){
+    return 0
+  }
+  var maxim = array[0]
+  for(var i = 0; i < array.length; i++){
+      if(array[i] > maxim){
+
+          maxim = array[i]
+
+      }
+  }
+  return maxim
 }
 
 exports.avg = function avg (array) {
-  return 0;
+  if(array == undefined || array.length == 0){
+      return 0
+  }
+  var summ = 0
+  for(var i = 0; i < array.length; i++){
+    summ = (summ + array[i])
+  }
+  return summ / array.length
 }
